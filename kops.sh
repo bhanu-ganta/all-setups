ad045859129781a0269d220d08f66bbb2dc5a008
bhanu-ganta <gantabhanuv@gmail.com>
2025-02-13 20:40:32 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,5 +14,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket bhanus.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket bhanus.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://bhanus.k8s.local
-kops create cluster --name bhanus.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+kops create cluster --name bhanus.k8s.local --zones us-east-1a --master-count=1 --master-size t2.micro --node-count=2 --node-size t2.medium
 kops update cluster --name bhanus.k8s.local --yes --admin
